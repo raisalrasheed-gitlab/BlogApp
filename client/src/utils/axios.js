7b53a7479315ee086833de6e141f45100bbf63a0
Raisal Rasheed <raisalrasheed545@gmail.com>
2025-02-02 17:41:42 +0530

    Update axios.js

--- client/src/utils/axios.js
+++ client/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:9001/',
+  baseURL: 'https://blog-app-x2vy.vercel.app/',
 });
 
 export default axiosInstance;
